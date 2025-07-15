a00e36bf54f7c6ba5ecb9e577370ba0860b7183d
Alan Strauhs <scarydoorsyeah@gmail.com>
2025-07-15 22:40:22 +0100

    i figured out why client closed is not true when context gets cancelled...

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
 func run(ctx context.Context) error {
+	ctx, cancel := context.WithCancel(ctx)
 	cl, err := kgo.NewClient(
 		kgo.SeedBrokers("localhost:9094"),
 		kgo.ConsumerGroup("franz-go-group"),
@@ -23,6 +25,36 @@ func run(ctx context.Context) error {
 		return err
 	}
 
+	go consume(ctx, cl)
+
+	time.Sleep(5 * time.Second)
+	cancel()
+	cl.Close()
+	fmt.Println("should have closed client")
+	time.Sleep(5 * time.Second)
+
+	return nil
+}
+
+func consume(ctx context.Context, cl *kgo.Client) error {
+	outer: for {
+		fetches := cl.PollFetches(ctx)
+		if fetches.IsClientClosed() {
+			break
+		}
+
+		select {
+		case <-ctx.Done():
+			fmt.Printf("%v", fetches.Errors())
+			break outer
+		default:
+		}
+
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("still inside loop")
+	}
+
+	fmt.Println("escaped loop")
 	return nil
 }
 
